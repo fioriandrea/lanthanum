d74bc01412031be8d13a8a1e9a72d8cda56012f4
fioriandrea <andrea.fiori.1998@gmail.com>
2020-09-02 11:38:12 +0200

    disable debug switches

--- src/debug/debug_switches.h
+++ src/debug/debug_switches.h
@@ -1,7 +1,7 @@
 #ifndef debug_switches
 #define debug_switches
 
-#define DEBUG_ON
+//#define DEBUG_ON
 
 #ifdef DEBUG_ON
 
@@ -11,15 +11,15 @@
 #include "map_printer.h"
 #include "value_dump.h"
 
-//#define STRESS_GC
-//#define TRACE_GC
-//#define TRACE_OBJECT_LIST
-//#define TRACE_TOKENS 
-//#define PRINT_CODE
-//#define TRACE_EXEC
-//#define TRACE_OPEN_UPVALUES
-//#define TRACE_INTERNED
-//#define TRACE_GLOBALS
+#define STRESS_GC
+#define TRACE_GC
+#define TRACE_OBJECT_LIST
+#define TRACE_TOKENS 
+#define PRINT_CODE
+#define TRACE_EXEC
+#define TRACE_OPEN_UPVALUES
+#define TRACE_INTERNED
+#define TRACE_GLOBALS
 
 #endif
 
